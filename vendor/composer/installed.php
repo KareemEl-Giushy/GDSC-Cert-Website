32145a7933afd55196c65fef1b85ad600337becf
Kareem El-Giushy <kareem.64123@gmail.com>
2021-11-27 01:42:01 +0200

    OM Elli Gab El Deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3e1fa04a477c8b03d188ca6e90eb961cce3e8b07',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e1fa04a477c8b03d188ca6e90eb961cce3e8b07',
+    ),
+  ),
+);
